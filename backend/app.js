c50ae4b3b263b8f48a548b2cf53af8c61f89c1e4
Your Name <your-email@example.com>
2025-04-17 12:27:45 +0100

    fix: clean up CSP header to allow only self-hosted resources

--- backend/app.js
+++ backend/app.js
@@ -8,15 +8,16 @@ const eventRoutes = require("./routes/events");
 
 const app = express();
 app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'self'; style-src 'self' [https://fonts.googleapis.com](https://fonts.googleapis.com); font-src 'self' [https://fonts.gstatic.com](https://fonts.gstatic.com); img-src 'self' data:; script-src 'self';");
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "default-src 'self'; style-src 'self'; font-src 'self'; img-src 'self' data:; script-src 'self';");
+  next();
+});
   next();
 });
 app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'self'; style-src 'self' [https://fonts.googleapis.com](https://fonts.googleapis.com); font-src 'self' [https://fonts.gstatic.com](https://fonts.gstatic.com); img-src 'self' data:; script-src 'self';");
   next();
 });
 app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'self'; style-src 'self' [https://fonts.googleapis.com](https://fonts.googleapis.com); font-src 'self' [https://fonts.gstatic.com](https://fonts.gstatic.com); img-src 'self' data:; script-src 'self';");
   next();
 });
 const PORT = process.env.PORT || 5000;
